7a64c603b674964d9e4d6c8e9aa9d0be1d9b42a9
Greb-the-awesome <82245536+Greb-the-awesome@users.noreply.github.com>
2024-03-07 13:39:48 -0500

    how are my commits failing

--- betrtetr.js
+++ betrtetr.js
@@ -1,24 +1,16 @@
-/*
-* old tetris code.
-* we will revise this code and add:
- * dark theme
- * soft drop
- * lock delay
- * nice GUI
-* in the next meeting
-*/
-
 var canvas = document.getElementById("canv");
 var ctx = canvas.getContext("2d");
 var w = 300, h = 600;
 var boardWidth = 10, boardHeight = 20;
 var squareWidth = w / boardWidth;
 var grid = [];
+var onDelay = false;
 var hold = "O";
 var alreadyHeld = false;
+var lastClear = 0;
 
 // 7-bag piece generator
-var bag = [];
+var bag = [], nextBag = [];
 var queue = [];
 function _getPiece() {
     if (bag.length == 0) {
@@ -127,6 +119,7 @@ addEventListener("keydown", function(e) {
             }
         }
         alreadyHeld = false;
+        onDelay = false;
     }
 
     // hold
@@ -140,19 +133,41 @@ addEventListener("keydown", function(e) {
         }
         alreadyHeld = true;
     }
-})
+});
+
+function displayMessage(msg, x, y) {
+    var elem = document.getElementById("statusholder");
+    var n = document.createElement("h1");
+    setTimeout(function() {n.remove();}, 2400);
+    n.appendChild(document.createTextNode(msg));
+    elem.appendChild(n);
+    n.style.position = "absolute";
+    n.style.left = x + "px"; n.style.top = y + "px";
+    n.style.animation = "fadeout 2.5s";
+}
 
 function gameLoop() {
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 600, 600); // clear with black
     
     ctx.font = "30px Comic Sans";
     if (alreadyHeld) {
         ctx.fillStyle = "grey";
     } else {
-        ctx.fillStyle = "green";
+        ctx.fillStyle = "#00FFFF";
     }
-    ctx.fillText("hold: " + hold + ", next: " + queue, 0, squareWidth);
+    ctx.fillText("HOLD", (boardWidth + 1) * squareWidth, squareWidth);
+    for (var coord of pieces[hold][0]) {
+        ctx.fillRect((coord[0] + boardWidth + 1) * squareWidth, (coord[1] + 1) * squareWidth, squareWidth, squareWidth);
+    }
+    ctx.fillStyle = "white";
+    ctx.fillText("NEXT", (boardWidth + 1) * squareWidth, 6 * squareWidth);
+    // show next 5 pieces
+    for (var i=0; i<5; i++) {
+        for (var coord of pieces[queue[i]][0]) {
+            ctx.fillRect((coord[0] + boardWidth) * squareWidth, (coord[1] + 6 + i * 5) * squareWidth, squareWidth, squareWidth);
+        }
+    }
     ctx.strokeStyle = "lightgrey";
     for (var i=0; i<=w; i+=squareWidth) {
         ctx.moveTo(i, 0);
@@ -162,7 +177,7 @@ function gameLoop() {
         ctx.moveTo(0, i);
         ctx.lineTo(w, i);
     }
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "grey";
     ctx.stroke();
     for (var i=0; i<boardWidth; i++) {
         for (var j=0; j<boardHeight; j++) {
@@ -172,7 +187,7 @@ function gameLoop() {
         }
     }
     var freeze = false;
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "#00FF00";
     for (var coord of pieces[currentPiece][currentRotation]) {
         ctx.fillRect((coord[0] + pieceX) * squareWidth, (coord[1] + pieceY) * squareWidth, squareWidth, squareWidth);
         if (coord[1] + pieceY + 1 >= boardHeight || grid[coord[0]+pieceX][coord[1]+pieceY+1]) {
@@ -180,26 +195,32 @@ function gameLoop() {
             freeze = true;
         }
     }
-    if (freeze) {
-        // freeze the piece and put a new one
-        for (var coord of pieces[currentPiece][currentRotation]) {
-            grid[coord[0] + pieceX][coord[1] + pieceY] = true;
-        }
-        pieceY = 0; pieceX = 3;
-        currentPiece = getPiece();
-        // check if this new piece is obstructed
-        for (var coord of pieces[currentPiece][currentRotation]) {
-            if (grid[coord[0]+pieceX][coord[1]+pieceY]) {
-                clearInterval(gameInterval);
-                ctx.font = "100px Calibri";
-                ctx.fillStyle = "red";
-                ctx.fillText("YOU LOSE bum", 0, 100);
+    if (freeze && !onDelay) {
+        onDelay = true;
+        setTimeout(function() {
+            if (!onDelay) return;
+            onDelay = false;
+            // freeze the piece and put a new one
+            for (var coord of pieces[currentPiece][currentRotation]) {
+                grid[coord[0] + pieceX][coord[1] + pieceY] = true;
             }
-        }
-        alreadyHeld = false;
+            pieceY = 0; pieceX = 3;
+            currentPiece = getPiece();
+            // check if this new piece is obstructed
+            for (var coord of pieces[currentPiece][currentRotation]) {
+                if (grid[coord[0]+pieceX][coord[1]+pieceY]) {
+                    clearInterval(gameInterval);
+                    ctx.font = "100px Calibri";
+                    ctx.fillStyle = "red";
+                    ctx.fillText("YOU LOSE bum", 0, 100);
+                }
+            }
+            alreadyHeld = false;
+        }, 500);
     }
 
     // clear line
+    var linesCleared = 0;
     for (var i=0; i<boardHeight; i++) {
         var lineCleared = true;
         for (var j=0; j<boardWidth; j++) {
@@ -210,10 +231,19 @@ function gameLoop() {
                 grid[j].splice(i, 1);
                 grid[j].unshift(false);
             }
+            linesCleared++;
         }
+    }
+    if (linesCleared) {
+        displayMessage(["SINGLE", "DOUBLE", "TRIPLE", "QUAD"][linesCleared-1], 300, 100);
+        if (linesCleared == lastClear && linesCleared > 1) {
+            displayMessage("B2B" + ["SINGLE", "DOUBLE", "TRIPLE", "QUAD"][linesCleared-1], 300, 200);
+        }
+        lastClear = linesCleared;
     }
 }
 
 var gameInterval = setInterval(gameLoop, 16.666);
 
-setInterval(function() {pieceY++;}, 1000);
+setInterval(function() {if (onDelay || downKeys["ArrowDown"]) return; pieceY++;}, 1000);
+setInterval(function() {if (onDelay || !downKeys["ArrowDown"]) return; pieceY++;}, 75);
